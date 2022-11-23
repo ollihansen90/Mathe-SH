c4d46a167398973db375affb1d067a7334fb9d38
ollihansen90 <50058898+ollihansen90@users.noreply.github.com>
2022-11-23 15:28:29 +0100

    Create komplex.py

---
+++ utils/komplex.py
@@ -0,0 +1,20 @@
+class Komplex():
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def add(self, zahl):
+        return Komplex(self.real+zahl.real, self.imag+zahl.imag)
+
+    def sub(self, zahl):
+        return Komplex(self.real-zahl.real, self.imag-zahl.imag)
+
+    def mul(self, zahl):
+        r = self.real*zahl.real-self.imag*zahl.imag
+        i = self.real*zahl.imag+self.imag*zahl.real
+        return Komplex(r, i)
+
+    def div(self, zahl):
+        r = (self.real*zahl.real+self.imag*zahl.imag)/(zahl.real**2+zahl.imag**2)
+        i = (self.imag*zahl.real-self.real*zahl.imag)/(zahl.real**2+zahl.imag**2)
+        return Komplex(r, i)
